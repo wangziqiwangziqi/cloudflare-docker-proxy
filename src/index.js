2811312b0ca701f2027a45f35f3ad3d18a6eb193
qist <87984115@qq.com>
2024-06-07 16:42:24 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.tycng.com": "https://registry-1.docker.io",
+  "quay.tycng.com": "https://quay.io",
+  "gcr.tycng.com": "https://gcr.io",
+  "k8s-gcr.tycng.com": "https://k8s.gcr.io",
+  "k8s.tycng.com": "https://registry.k8s.io",
+  "ghcr.tycng.com": "https://ghcr.io",
+  "cloudsmith.tycng.com": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
